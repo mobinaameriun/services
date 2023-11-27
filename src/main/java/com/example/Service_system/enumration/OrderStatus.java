169812f4c1e6402e818941c98aaf5cc4618ee3ef
top <mobinaamerion@gmail.com>
2023-11-27 15:25:54 +0330

    add enumeration

---
+++ src/main/java/com/example/Service_system/enumration/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.example.Service_system.enumration;
+
+public enum OrderStatus {
+    WAITING_FOR_THE_SUGGESTION_OF_EXPERT,
+    WAITING_FOR_PROFICIENT_SELECTION,
+    WAITING_FOR_THE_PROFICIENT_TO_COME_TO_YOUR_PLACE,
+    STARTED,
+    COMPLETED,
+    PAID;
+
+}
