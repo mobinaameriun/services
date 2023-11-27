f997a9a384101843f136f4c86038904528732a18
top <mobinaamerion@gmail.com>
2023-11-27 15:25:16 +0330

    add model

---
+++ src/test/java/com/example/Service_system/ServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
